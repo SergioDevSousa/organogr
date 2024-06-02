96a10ccb455126a178c9518a1e9ab21589f9fd4e
SergioDevSousa <166451865+SergioDevSousa@users.noreply.github.com>
2024-06-01 22:58:50 -0300

    atualização aula 2

---
+++ src/componentes/ListasSuspensas/index.js
@@ -0,0 +1,15 @@
+import './ListasSuspensa.css'
+
+const ListasSuspensas = (props) =>{
+    console.log(props.itens)
+    return(
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select required={props.required}>
+                {props.itens.map(item => {
+                return <option key={item}>{item}</option>})}
+            </select>
+        </div>
+    )
+}
+export default ListasSuspensas
--- src/componentes/formulario/index.js
+++ src/componentes/formulario/index.js
@@ -1,14 +1,37 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
+import ListasSuspensas from '../ListasSuspensas'
 import './Form.css'
 
+
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Designer',
+        'Mobile',
+        'Inovação eGestão'
+    ]
+
+const aoSalvar = (evento) =>{
+    evento.preventDefault()
+    console.log('Form foi submetido')
+}
+
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados pra criar o card do Colaborador. </h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome..." />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo..." />
-                <CampoTexto label="Imagem" placeholder="Digite o caminho da sua imagem..." />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome..." />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo..." />
+                <CampoTexto obrigatorio={true} label="Imagem" placeholder="Digite o caminho da sua imagem..." />
+                <ListasSuspensas obrigatorio={true} label="Time" itens = {times}/>
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
